7e7b34dc889cfd32fa740f94e47458dd1ee70be2
p-iknow <apricotsoul@gmail.comm>
2022-07-25 09:00:55 +0900

    feat: 꼭 필요한 프로그램만 남기고 지우기

--- init.lua
+++ init.lua
@@ -5,8 +5,7 @@
 local inputEnglish = "com.apple.nlayout.ABC"
 local inputKorean = "com.apple.keylayout.2SetHangul"
 
-local app_man = require('modules.appman')
-local app_mode = hs.hotkey.modal.new()
+
 
 hs.window.animationDuration = 0
 
@@ -34,67 +33,45 @@ function app_toggle(name, secondName)
     end
 end
 
-hs.hints.hintChars = {
-    'q', 'w', 'e', 'r',
-    'a', 's', 'd', 'f',
-    'z', 'x', 'c', 'v',
-    '1', '2', '3', '4',
-    'j', 'k',
-    'i', 'o',
-    'm', ','
-}
-
-
-
 local event_map = {
-    -- hammerspoon 관리
-    { key = 'r', mod = { 'shift' }, func = hs.reload },
+    { key = 'r', mod = 'shift', func = hs.reload },
     -- app_toggle
-    { key = 'n', mod = {}, func = app_toggle('Notion') },
-    { key = 'm', mod = {}, func = app_toggle('Google Meet') },
-    { key = 'c', mod = {}, func = app_toggle('Google Chrome') },
-    { key = 'd', mod = {}, func = app_toggle('discord') },
-    { key = 'f', mod = {}, func = app_toggle('Finder') },
-    { key = 'k', mod = {}, func = app_toggle('KakaoTalk') },
-    { key = 'p', mod = {}, func = app_toggle('PDF Expert') },
-    { key = 'r', mod = {}, func = app_toggle('draw.io') },
-    { key = 's', mod = {}, func = app_toggle('Slack') },
-    { key = 'v', mod = {}, func = app_toggle('Visual Studio Code') },
-    { key = 'space', mod = {}, func = app_toggle('iTerm') },
-    { key = 'tab', mod = {}, func = hs.hints.windowHints },
-    { key = 'tab', mod = { 'shift' }, func = hs.window._timed_allWindows },
+    { key = 'n', mod = "", func = app_toggle('Notion') },
+    { key = 'm', mod = "", func = app_toggle('Google Meet') },
+    { key = 'c', mod = "", func = app_toggle('Google Chrome') },
+    { key = 'd', mod = "", func = app_toggle('discord') },
+    { key = 'f', mod = "", func = app_toggle('Finder') },
+    { key = 'k', mod = "", func = app_toggle('KakaoTalk') },
+    { key = 'p', mod = "", func = app_toggle('PDF Expert') },
+    { key = 'r', mod = "", func = app_toggle('draw.io') },
+    { key = 's', mod = "", func = app_toggle('Slack') },
+    { key = 'v', mod = "", func = app_toggle('Visual Studio Code') },
+    { key = 'space', mod = "", func = app_toggle('iTerm') },
 }
 
-do
-    local event_runner = require('modules.event_runner')
-    event_runner:init('f4', event_map)
-end
-
-
--- spoon plugins
-hs.loadSpoon("SpoonInstall")
-spoon.SpoonInstall.use_syncinstall = false
-
-function plugInstall()
-    local Install = spoon.SpoonInstall
-    Install:updateRepo('default')
-
-    hs.alert.show('plugin installed')
-end
-
-require('modules.inputsource_aurora')
-
-function dump(o)
-    if type(o) == 'table' then
-        local s = '{ '
-        for k, v in pairs(o) do
-            if type(k) ~= 'number' then k = '"' .. k .. '"' end
-            s = s .. '[' .. k .. '] = ' .. dump(v) .. ','
-        end
-        return s .. '} '
-    else
-        return tostring(o)
+function event_runner(func_table)
+    for i, v in pairs(func_table) do
+        -- hs.alert.show(i)
+        local key = v['key']
+        local mod = v['mod']
+        local func = v['func']
+        hs.hotkey.bind({ 'option', mod }, key, func)
     end
 end
 
-hs.alert.show('loaded')
+event_runner(event_map)
+
+function reloadConfig(files)
+    doReload = false
+    for _, file in pairs(files) do
+        if file:sub(-4) == ".lua" then
+            doReload = true
+        end
+    end
+    if doReload then
+        hs.reload()
+    end
+end
+
+myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
+hs.alert.show("Config loaded")
